33b9121deff63afb5e9ed74bd8ef71901e487e1b
Oscar <COnCh3Tuxd>
2018-04-26 21:01:53 -0500

    prueba server new Conexion

--- inc/data/class/Conexion.php
+++ inc/data/class/Conexion.php
@@ -1,16 +1,54 @@
 
-
 <?php
-	session_start();
-	class Conexion {
-		public static function cnx()
-		{			
-			$cnn=mysql_connect("localhost","oscar","Ch3Tu123");
-				 mysql_query("SET NAMES utf8");
-				 mysql_select_db("sampledb"); 			
-			return $cnn;
+/*
+* Mysql database class - only one connection alowed
+*/
+class Conexion {
+	private $_connection;
+	private static $_instance; //The single instance
+	private $_host = "localhost";
+	private $_username = "root";
+	private $_password = "root";
+	private $_database = "sampledb";
+	/*
+	Get an instance of the Database
+	@return Instance
+	*/
+	public static function getInstance() {
+		if(!self::$_instance) { // If no instance then make one
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+	// Constructor
+	private function __construct() {
+		$this->_connection = new mysqli($this->_host, $this->_username, 
+			$this->_password, $this->_database);
+	
+		// Error handling
+		if(mysqli_connect_error()) {
+			trigger_error("Failed to conencto to MySQL: " . mysql_connect_error(),
+				 E_USER_ERROR);
 		}
 	}
+	// Magic method clone is empty to prevent duplication of connection
+	private function __clone() { }
+	// Get mysqli connection
+	public function getCnx() {
+		return $this->_connection;
+	}
+}
+?>
+
+<?php
+
+
+ 	$db = Conexion::getInstance();
+    $mysqli = $db->getCnx(); 
+    $sql_query = "SELECT * FROM db_nivel";
+    $result = $mysqli->query($sql_query);
+
+    print_r($result);
 ?>
 
 
