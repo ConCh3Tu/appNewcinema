f4b9cf14d1939852b3db55c3a8fdfd666bab1427
Oscar <COnCh3Tuxd>
2018-04-26 20:22:15 -0500

    modo COnexion user clave namedb

--- inc/data/class/Conexion.php
+++ inc/data/class/Conexion.php
@@ -1,14 +1,17 @@
+
+
 <?php
 	session_start();
 	class Conexion {
 		public static function cnx()
 		{			
-			$cnn=mysql_connect("localhost","root","root");
+			$cnn=mysql_connect("localhost","oscar","Ch3Tu123");
 				 mysql_query("SET NAMES utf8");
-				 mysql_select_db("videocine"); 			
+				 mysql_select_db("sampledb"); 			
 			return $cnn;
 		}
 	}
 ?>
+
 
 
